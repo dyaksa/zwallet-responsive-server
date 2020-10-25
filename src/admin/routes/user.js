86aa8e56282016d26224e3bf948c5a3300afabba
ayuputrii <ayuarmadani.pm@gmail.com>
2020-10-25 19:33:49 +0700

    Change Admin User

---
+++ src/admin/routes/user.js
@@ -0,0 +1,13 @@
+const router = require("express").Router();
+const adminController = require("../admin/controllers-admin/user");
+const { authentication, authorization } = require("../middlewares/auth");
+const upload = require("../middlewares/multer");
+
+router
+  .get("/search", authentication, adminController.searchAllUser)
+  .get("/search/receiver", authentication, adminController.searchOneById)
+  .get("/search/query", authentication, adminController.searchByName)
+  .get("/", authentication, authorization, adminController.getAllUser)
+  .patch("/", upload, authentication, adminController.editUser);
+
+module.exports = router;
