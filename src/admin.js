ded804080dfbb10de1609a4eb8874cd3116e80f9
rizkyagung18 <55870446+rizkyagung18@users.noreply.github.com>
2020-10-26 20:13:46 +0700

    looks good

--- src/admin.js
+++ src/admin.js
@@ -1,9 +1,13 @@
 const router = require("express").Router();
-const authRoutes = require("./routes/auth");
+// const authRoutes = require("./routes/auth");
 
 const topupRoutes = require("./admin/routes/topup");
 const adminRoutes = require("./admin/routes/user");
+const transferRoutes = require("./admin/routes/transfer");
 
-router.use("/topup", topupRoutes).use("/", adminRoutes);
+router
+  .use("/topup", topupRoutes)
+  .use("/users", adminRoutes)
+  .use("/transfer", transferRoutes);
 
 module.exports = router;
--- src/admin/routes/user.js
+++ src/admin/routes/user.js
@@ -4,12 +4,6 @@ const { authentication, authorization } = require("../../middlewares/auth");
 const upload = require("../../middlewares/multer");
 
 router
-  .get("/search", authentication, authorization, adminController.searchAllUser)
-  .get("/search/receiver", authentication, authorization, adminController.searchOneById)
-  .get("/search/query", authentication, authorization, adminController.searchByName)
-  .get("/", authentication, authorization, adminController.getAllUser)
-  .patch("/", upload, authentication, authorization, adminController.editUser);
-
   .get("/search/query", authentication, adminController.searchByName)
   .get("/", authentication, authorization, adminController.getAllUser)
   .patch("/", upload, authentication, adminController.editUser)
