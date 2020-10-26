6c004e9bef522f096f902b5baf432f4722771e23
rizkyagung18 <55870446+rizkyagung18@users.noreply.github.com>
2020-10-26 20:10:08 +0700

    authorization

--- src/admin/routes/topup.js
+++ src/admin/routes/topup.js
@@ -3,7 +3,7 @@ const topupController = require('../controllers/topup')
 const { authentication, authorization } = require('../../middlewares/auth')
 
 router
-    .get('/', authentication, topupController.getAllTopUp)
+    .get('/', authentication, authorization, topupController.getAllTopUp)
     .get('/:order', authentication, authorization, topupController.getTopUpByOrder)
     .post('/', authentication, authorization, topupController.postTopUp)
     .patch('/:order', authentication, authorization, topupController.editTopUp)
--- src/admin/routes/user.js
+++ src/admin/routes/user.js
@@ -4,10 +4,10 @@ const { authentication, authorization } = require("../../middlewares/auth");
 const upload = require("../../middlewares/multer");
 
 router
-  .get("/search", authentication, adminController.searchAllUser)
-  .get("/search/receiver", authentication, adminController.searchOneById)
-  .get("/search/query", authentication, adminController.searchByName)
+  .get("/search", authentication, authorization, adminController.searchAllUser)
+  .get("/search/receiver", authentication, authorization, adminController.searchOneById)
+  .get("/search/query", authentication, authorization, adminController.searchByName)
   .get("/", authentication, authorization, adminController.getAllUser)
-  .patch("/", upload, authentication, adminController.editUser);
+  .patch("/", upload, authentication, authorization, adminController.editUser);
 
 module.exports = router;
